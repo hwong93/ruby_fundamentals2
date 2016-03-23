742e6073a6e6ff35aa95582066ec5b50354fe3d5
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-03-23 15:07:39 -0400

    added cohort4 and reprinted

--- exercise7.rb
+++ exercise7.rb
@@ -3,7 +3,15 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
-
-students.map do |keys, values|
-  puts "#{keys}: #{values} students"
+# keys method to output all cohort names and students
+def keys(hash)
+  hash.map do |keys, values|
+    puts "#{keys}: #{values} students"
+  end
 end
+
+keys(students)
+
+# adding cohort 4
+students[:cohort4] = 43
+keys(students)
