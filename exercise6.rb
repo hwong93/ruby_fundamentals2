6688f911a4049af443387cda1990a4434130abfd
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-03-23 14:13:39 -0400

    added method to list items and to add items into the array

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,13 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |item|
-  puts "* #{item}\n"
+# method to list all the items with * and a new line
+def listitems(grocery_arr)
+    grocery_arr.to_a.each do |item|
+      puts "* #{item}\n"
+    end
 end
+
+listitems(grocery_list)
+# shovel or push in a new item "rice"
+grocery_list << "rice"
+listitems(grocery_list)
